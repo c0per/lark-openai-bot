cbffa69177ceae8f70a4ece0dbd0136483b48625
c0per <60544162+c0per@users.noreply.github.com>
2023-03-16 09:04:13 +0800

    feat: echo bot

--- src/lark/mod.rs
+++ src/lark/mod.rs
@@ -1,5 +1,41 @@
-use axum::{http::StatusCode, routing::post, Json, Router};
+use axum::{extract::State, http::StatusCode, routing::post, Json, Router};
 use serde_json::{self, json, Map};
+
+use crate::AppState;
+
+async fn handle_message(state: AppState, event: &Map<String, serde_json::Value>) {
+    let open_id = event
+        .get("sender")
+        .unwrap()
+        .get("sender_id")
+        .unwrap()
+        .get("open_id")
+        .unwrap()
+        .as_str()
+        .unwrap();
+
+    let content = event
+        .get("message")
+        .unwrap()
+        .get("content")
+        .unwrap()
+        .as_str()
+        .unwrap();
+
+    let client = reqwest::Client::new();
+    client
+        .post("https://open.feishu.cn/open-apis/im/v1/messages")
+        .bearer_auth(state.lock().await.tenant_token.get_token().await)
+        .query(&[("receive_id_type", "open_id")])
+        .json(&json!({
+            "receive_id": open_id,
+            "msg_type": "text",
+            "content": content
+        }))
+        .send()
+        .await
+        .unwrap();
+}
 
 fn handle_verification(
     payload: Map<String, serde_json::Value>,
@@ -10,11 +46,28 @@ fn handle_verification(
     }
 }
 
-fn handle_event() -> Result<Json<serde_json::Value>, StatusCode> {
+async fn handle_event_v2(
+    state: AppState,
+    payload: Map<String, serde_json::Value>,
+) -> Result<Json<serde_json::Value>, StatusCode> {
+    let header = payload.get("header").unwrap().as_object().unwrap();
+    let event = payload.get("event").unwrap().as_object().unwrap();
+
+    let event_type = header.get("event_type").unwrap().as_str().unwrap();
+    match event_type {
+        "im.message.receive_v1" => handle_message(state, event).await,
+        _ => unimplemented!(),
+    }
+
+    Ok(Json(serde_json::Value::Null))
+}
+
+fn handle_event_v1() -> Result<Json<serde_json::Value>, StatusCode> {
     todo!()
 }
 
 async fn post_event(
+    State(state): State<AppState>,
     Json(payload): Json<serde_json::Value>,
 ) -> Result<Json<serde_json::Value>, StatusCode> {
     let serde_json::Value::Object(payload) = payload else {
@@ -28,10 +81,13 @@ async fn post_event(
     {
         handle_verification(payload)
     } else {
-        handle_event()
+        match payload.get("schema") {
+            Some(_) => handle_event_v2(state, payload).await,
+            None => handle_event_v1(),
+        }
     }
 }
 
-pub(super) fn router() -> Router {
+pub(super) fn router() -> Router<AppState> {
     Router::new().route("/event", post(post_event))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,87 @@
 use axum::Router;
+use serde_json::json;
+use std::{
+    env,
+    sync::Arc,
+    time::{SystemTime, UNIX_EPOCH},
+};
+use tokio::sync::Mutex;
 
 mod lark;
 
+#[derive(Clone)]
+struct TenantToken {
+    token: String,
+    expire_timestamp: u64,
+}
+
+impl TenantToken {
+    async fn init() -> TenantToken {
+        get_tenant_token().await
+    }
+
+    async fn get_token(&mut self) -> &str {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        if now > self.expire_timestamp - 30 * 60 {
+            *self = get_tenant_token().await;
+        }
+        &self.token
+    }
+}
+
+#[derive(Clone)]
+struct AppStateInner {
+    tenant_token: TenantToken,
+}
+
+type AppState = Arc<Mutex<AppStateInner>>;
+
+async fn get_tenant_token() -> TenantToken {
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal")
+        .json(&json!({
+            "app_id": env::var("LARK_APP_ID").unwrap(),
+            "app_secret": env::var("LARK_APP_SECRET").unwrap()
+        }))
+        .send()
+        .await
+        .unwrap();
+
+    let json = res.json::<serde_json::Value>().await.unwrap();
+    let json = json.as_object().unwrap();
+
+    let token = json
+        .get("tenant_access_token")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_string();
+    let expiration = json.get("expire").unwrap().as_u64().unwrap();
+
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    TenantToken {
+        token,
+        expire_timestamp: now + expiration,
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().nest("/lark", lark::router());
+    let app = Router::new().nest(
+        "/lark",
+        lark::router().with_state(Arc::new(Mutex::new(AppStateInner {
+            tenant_token: TenantToken::init().await,
+        }))),
+    );
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
